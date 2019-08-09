ab351f4b640b59c23b67f24f5914369dbf7e3022
t-eguchi <t-eguchi@nssys.co.jp>
2019-08-09 11:37:08 +0900

    update

--- src/com/egu/sample/database/entity/Employee.java
+++ src/com/egu/sample/database/entity/Employee.java
@@ -1,5 +1,34 @@
 package com.egu.sample.database.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 社員を表すエンティティです。
+ * @author t-eguchi
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
+	/** 社員番号 */
+	private String no;
+
+	/** 氏名 */
+	private String name;
+
+	/** ふりがな */
+	private String kana;
+
+	/** ID */
+	private String id;
+
+	/** 所属 */
+	private String belong;
+
+	/** メールアドレス */
+	private String email;
 }
