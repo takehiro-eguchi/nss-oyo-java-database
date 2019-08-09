d46934db71f5eddda07059f7bb0fb163a1fd18f8
t-eguchi <t-eguchi@nssys.co.jp>
2019-08-09 13:12:44 +0900

    update

---
+++ src/com/egu/sample/database/integrate/EmployeeCsvAccessor.java
@@ -0,0 +1,38 @@
+package com.egu.sample.database.integrate;
+
+import java.util.List;
+
+import com.egu.sample.database.entity.Employee;
+
+/**
+ * CSVファイルを用いて、社員にアクセスする実装オブジェクトです。
+ * @author t-eguchi
+ *
+ */
+class EmployeeCsvAccessor implements EmployeeAccessor {
+
+	@Override
+	public void insert(Employee employee) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void delete(Employee employee) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public List<Employee> all() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public List<Employee> select(String text) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
